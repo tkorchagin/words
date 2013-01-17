f2045621fb643871791aebfe1ea248b085df9cc2
ASPCartman <aspcartman@gmail.com>
2013-01-17 05:18:34 +0400

    Dicitonaries made JSON-like. + MainViewController

---
+++ Words/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Words
+//
+//  Created by ASPCartman on 17.01.13.
+//  Copyright (c) 2013 Timofey Korchagin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ Words/MainViewController.m
@@ -0,0 +1,32 @@
+//
+//  MainViewController.m
+//  Words
+//
+//  Created by ASPCartman on 17.01.13.
+//  Copyright (c) 2013 Timofey Korchagin. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+@property (strong,nonatomic) NSDictionary *dictionaries;
+@end
+
+@implementation MainViewController
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+	if (self = [super initWithCoder:aDecoder])
+	{
+		
+	}
+	
+	return self;
+}
+
+- (NSDictionary*) loadDictionaries
+{
+	
+}
+
+@end
