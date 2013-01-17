c7d01ba9216c3e152d9ebb1df16a808c524870f0
ASPCartman <aspcartman@gmail.com>
2013-01-17 06:04:41 +0400

    Renamed dictionaries Method for parsing dictionaries

--- Words/MainViewController.m
+++ Words/MainViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "MainViewController.h"
+#import <JSONKit/JSONKit.h>
+#import <BlocksKit/BlocksKit.h>
 
 @interface MainViewController ()
 @property (strong,nonatomic) NSDictionary *dictionaries;
@@ -18,7 +20,7 @@
 {
 	if (self = [super initWithCoder:aDecoder])
 	{
-		
+		self.dictionaries = [self loadDictionaries];
 	}
 	
 	return self;
@@ -26,7 +28,35 @@
 
 - (NSDictionary*) loadDictionaries
 {
+	NSError * __autoreleasing *error = nil;
+	NSString *dictJSONList = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Dictionaries" ofType:@"json" inDirectory:@"Dictionaries"]
+									   encoding:NSUTF8StringEncoding
+										error:error];
+	NSMutableDictionary *dictList = [dictJSONList mutableObjectFromJSONString];
 	
+	if (error)
+	{
+		NSLog(@"Can't open the dictionaries list");
+		return nil;
+	}
+	
+	BKKeyValueTransformBlock parseDict = ^id(NSString *key, NSString *obj)
+	{
+		NSError * __autoreleasing *error = nil;
+		
+		NSString *fileName = obj;
+		NSString *fileContent = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:fileName ofType:@"json" inDirectory:@"Dictionaries"]
+										  encoding:NSUTF8StringEncoding
+										     error:error];
+		if (error) return nil;
+		
+		NSArray *words = [fileContent objectFromJSONString];
+		return words;
+	};
+	
+	[dictList performMap:parseDict];
+
+	return dictList;
 }
 
 @end
