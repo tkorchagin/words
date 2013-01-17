8f0939fce637bf6eb9ce7104735d1e5c614e0baa
Timofey Korchagin <tkorchagin@gmail.com>
2013-01-17 04:07:22 +0400

    Initial Commit

---
+++ Words/TKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKAppDelegate.h
+//  Words
+//
+//  Created by Timofey Korchagin on 17/01/2013.
+//  Copyright (c) 2013 Timofey Korchagin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Words
+//
+//  Created by Timofey Korchagin on 17/01/2013.
+//  Copyright (c) 2013 Timofey Korchagin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TKAppDelegate class]));
+    }
+}
